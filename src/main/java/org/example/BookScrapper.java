3692a7f5501bdfccfc3b9f3ad2c848455607ded2
Gideon <kiplagatgideon@gmail.com>
2025-03-17 13:16:52 +0300

    Legally Scraped books. CSS Selector used

---
+++ src/main/java/org/example/BookScrapper.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class BookScrapper {
+    public static void main(String[] args) {
+        String url = "http://books.toscrape.com/";
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements books = doc.select(".product_pod");
+
+            for (Element book : books) {
+                String title = book.select("h3 > a").text();
+                String price = book.select(".price_color").text();
+                String pr = price.substring(1);
+
+                if(Double.parseDouble(pr) < 50) {
+                    System.out.println(title + " - " + price);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
